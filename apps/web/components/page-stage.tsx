190787c3a7d841e0314b6648fc2850a2da6ae57b
Jared Tucker <jntucker02@gmail.com>
2026-04-10 11:34:38 +1000

    Jesus

--- apps/web/components/page-stage.tsx
+++ apps/web/components/page-stage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function PageStage({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="relative mx-auto w-full lg:max-w-[min(100dvw,var(--breakpoint-3xl))]">
+      <div className=>
         {children}
       </div>
     </>
